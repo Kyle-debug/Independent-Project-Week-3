5cb7305a38545793a997f1adbeae2d9f4a89ca2a
Kyle Kioko <kylekioko.kk@gmail.com>
2020-05-18 08:06:44 +0300

    add js else

--- javascript/script.js
+++ javascript/script.js
@@ -87,3 +87,10 @@ $(document).ready(function(){
           if ($("input#MERGE1").val() && $("input#MERGE0").val()){
             alert (name + ", we have received your message. Thank you for reaching out to us.");
           }
+        else {
+            alert("Please enter your name and email!");
+          }
+          
+        });
+      
+      });
